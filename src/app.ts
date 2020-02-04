959790421cdd41e1b67802cce2dda48c2a8914b3
Andy0414 <pjh8667@naver.com>
2020-02-04 12:30:35 +0900

    Update express setting

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log("Hello World!")
+import * as express from "express";
+import * as cors from "cors";
+import * as helmet from "helmet";
+import * as compression from "compression";
+import * as morgan from "morgan";
+
+import "dotenv/config";
+
+const app: express.Application = express(); // 서버 객체
+
+app.use(morgan("dev")); // 개발용 로그 미들웨어
+app.use(cors()); // CORS 설정 미들웨어
+app.use(helmet()); // 보안 미들웨어
+app.use(compression()); // 데이터 압축 미들웨어
+
+app.use(express.static("public")); // public 폴더의 파일을 제공함
+app.use(express.urlencoded({ limit: "20mb" })); // urlencode 지원
+app.use(express.json({ limit: "20mb" })); // json 지원
+
+app.listen(process.env.PORT || 3000, () => {
+	// 서버가 열렸을 시 콜백
+	console.log(`PORT : ${process.env.PORT || 3000}`);
+});
+
+export default app;
