b600c8f702b1fbbd4b1f4bd9feaf895c47b266e2
Andy0414 <pjh8667@naver.com>
2020-04-08 00:27:25 +0900

    Update base64toimage

---
+++ src/modules/Base64-To-Image.ts
@@ -0,0 +1,29 @@
+export interface Base64Image {
+	imgFile: Buffer;
+	imgType: string;
+}
+class Base64ToImage {
+	/**
+	 * @description base64를 이미지파일로 변환합니다.
+	 * @param base64
+	 */
+	getImageData(base64: string) {
+		let splitData = base64.split(",");
+		let imgType = null;
+		if (splitData.length > 1) {
+			imgType = splitData[0].split(";")[0].split("/")[1];
+			switch (imgType) {
+				case "jpeg":
+					imgType = "jpg";
+					break;
+			}
+			base64 = splitData[1];
+		}
+		let imageBuffer = Buffer.from(base64, "base64");
+		return {
+			imgFile: imageBuffer,
+			imgType: imgType,
+		};
+	}
+}
+export default new Base64ToImage();
