e2a419b059b4e18c697b727c0df23e9acbabd3bd
Andy0414 <pjh8667@naver.com>
2020-04-20 00:50:31 +0900

    Update example

--- src/socket/auth/auth.socket.ts
+++ src/socket/auth/auth.socket.ts
@@ -1,5 +1,9 @@
 import { SocketRouter } from "../../modules/SocketIO-Manager";
 
-const socketRouter: SocketRouter = (io: SocketIO.Server, socket: SocketIO.Socket) => {};
+const socketRouter: SocketRouter = (io: SocketIO.Server, socket: SocketIO.Socket) => {
+	socket.on("ping", () => {
+		io.sockets.emit("pong", true);
+	});
+};
 
 export default socketRouter;
