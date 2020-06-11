78a421d326a0636bcc1f76e574b1d126af32b95d
Andy0414 <pjh8667@naver.com>
2020-06-11 12:11:05 +0900

    Update auth.ts env

--- config/auth.ts
+++ config/auth.ts
@@ -1,23 +1,23 @@
 export default {
 	loginRedirect: "/auth/users/my", // 세션 사용 시 로그인 시도 후 리다이렉션 되는 링크
 	github: {
-		clientID: "",
-		clientSecret: "",
+		clientID: "" || process.env.GITHUB_CLIENTID,
+		clientSecret: "" || process.env.GITHUB_CLIENTSECRET,
 		callbackURL: "/auth/users/login/github/callback",
 	},
 	naver: {
-		clientID: "",
-		clientSecret: "",
+		clientID: "" || process.env.NAVER_CLIENTID,
+		clientSecret: "" || process.env.NAVER_CLIENTSECRET,
 		callbackURL: "/auth/users/login/naver/callback",
 	},
 	google: {
-		clientID: "",
-		clientSecret: "",
+		clientID: "" || process.env.GOOGLE_CLIENTID,
+		clientSecret: "" || process.env.GOOGLE_CLIENTSECRET,
 		callbackURL: "/auth/users/login/google/callback",
 	},
 	kakao: {
-		clientID: "",
-		clientSecret: "",
+		clientID: "" || process.env.KAKAO_CLIENTID,
+		clientSecret: "" || process.env.KAKAO_CLIENTSECRET,
 		callbackURL: "/auth/users/login/kakao/callback",
 	},
 };
